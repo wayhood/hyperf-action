f5c3bfc1c1865eddb5e268e959d70f1d9c001d72
wayhood <wayhood@163.com>
2020-05-11 14:05:37 +0800

    fix

---
+++ src/Service/TokenService.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Wayhood\HyperfAction\Service;
+
+use Wayhood\HyperfAction\Contract\TokenInterface;
+
+/**
+ * Class TokenService
+ * @package App\Service
+ */
+class TokenService implements TokenInterface
+{
+    public function verify(string $token)
+    {
+        // TODO: Implement verify() method.
+    }
+
+    public function has(string $token)
+    {
+        // TODO: Implement has() method.
+    }
+
+    public function generator(array $value)
+    {
+        // TODO: Implement generator() method.
+    }
+
+    public function set(string $token)
+    {
+        // TODO: Implement set() method.
+    }
+
+    public function get(string $token)
+    {
+        // TODO: Implement get() method.
+    }
+
+}
