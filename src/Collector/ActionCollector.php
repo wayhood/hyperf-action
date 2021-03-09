9f5e43e31a9a6f06ef31c201c2c236afa2725251
wayhood <wayhood@wayhooddeMacBook-Pro.local>
2021-03-09 22:52:49 +0800

    fix

--- src/Collector/ActionCollector.php
+++ src/Collector/ActionCollector.php
@@ -20,8 +20,10 @@ class ActionCollector extends MetadataCollector
 
     public static function collectClass(string $class, string $annotation, $value): void
     {
-        $msg = "Duplicate definition Action(\"$value\") in ". static::$container[$value] .",". $class;
-        throw new \Exception($msg);
+        if (isset(static::$container[$value])) {
+            $msg = "Duplicate definition Action(\"$value\") in ". static::$container[$value] .",". $class;
+            throw new \Exception($msg);
+        }
         static::$container[$value] = $class;
     }
 
